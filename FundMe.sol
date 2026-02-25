45f209df6b88f2f3b5aff1cc78b2e4fe29421673
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-02-25 06:47:16 +0530

    hardcoded the fundme contract, still looking for things to improve

--- FundMe.sol
+++ FundMe.sol
@@ -1,4 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./PriceConverter.sol";
 
+contract FundMe {
+    using PriceConverter for uint256;
+    address payable public immutable owner;
+    address public priceFeed;
+    uint256 public amount;
+
+    address[] public funders;
+    mapping (address => uint) public addressToAmountFunded;
+
+    constructor(address _priceFeed, uint256 _amount) {
+        owner = payable(msg.sender);
+        priceFeed = _priceFeed;
+        amount = _amount;
+    }
+
+    modifier onlyOwner {
+        require(
+            msg.value.getConversionRate(priceFeed) >= amount,
+            "You need to spend more ETH!"
+        );
+        _;
+    }
+
+    function fund () public payable {
+        require(
+            msg.value.getConversionRate(priceFeed) >= amount,
+            "You need to spend more ETH!"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public payable onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success, "Transfer failed.");
+        for (uint256 i = 0; i < funders.length; i++) {
+            addressToAmountFunded[funders[i]] = 0;
+        }
+        funders = new address[](0);
+    }
+
+}
